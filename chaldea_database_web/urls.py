79d4cf4724ddba3b1facb241f301cc55d5ecb10b
yatabis <39yatabis.rim8820xxx@gmail.com>
2020-01-08 17:16:04 +0900

    update: servants list view

--- chaldea_database_web/urls.py
+++ chaldea_database_web/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ServantsListView
 
 urlpatterns = [
-    path('servants', ServantsListView.as_view(), name='servants'),
+    path('spirit-origin', ServantsListView.as_view(), name='servants'),
 ]
